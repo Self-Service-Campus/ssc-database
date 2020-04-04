82d3a92856b84453fac78224688d0e117a38e43f
tomas99batista <tomas99batista@gmail.com>
2020-04-04 02:05:00 +0100

    added __str__

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # database
+
+./manage.py graph_models -a --hide-edge-labels -o db_model.png
+
--- ssc_db/ssc_db_app/models.py
+++ ssc_db/ssc_db_app/models.py
@@ -5,18 +5,27 @@ class Department(models.Model):
     code = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return f"Departamento nº{code}: {name}"
+
 class User(models.Model):
     uu = models.CharField(max_length=100, primary_key=True)
     email_ua = models.EmailField()
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     department = models.OneToOneField(Department, on_delete=models.CASCADE) # tenho de adicionar alguma coisa on_delete?
+    
+    def __str__(self):
+        return f"User {uu}, @: {email_ua}, {first_name} {last_name}, dep: {department}"
 
 class Switch(models.Model):
     identifier = models.CharField(max_length=100, primary_key=True)
     model = models.CharField(max_length=100)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    user_acess = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f"Switch {identifier}, {model}, dep: {department}"
+
 
 class Port(models.Model):
     number = models.IntegerField(primary_key=True)
@@ -24,16 +33,30 @@ class Port(models.Model):
     state = models.CharField(max_length=100)
     switch = models.ForeignKey(Switch, on_delete=models.CASCADE) # on_delete?
 
+    def __str__(self):
+        return f"Port {number}, ip: {ip_addr}, state: {state}, from: {switch}"
+
+
 class VLAN(models.Model):
     identifier = models.IntegerField(primary_key=True)
     description = models.CharField(max_length=350)
     port = models.ForeignKey(Port, on_delete=models.CASCADE) # on_delete?
 
+    def __str__(self):
+        return f"VLAN {identifier}, {description}, port: {port}"
+
+
 class ACL(models.Model):
     id = models.AutoField(primary_key=True)
     access_flag = models.BooleanField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"ACL {id}, ip: {access_flag}, user: {user}"
 
 class Audit_Log(models.Model):
     id = models.AutoField(primary_key=True)
     action = models.CharField(max_length=350)
+
+    def __str__(self):
+        return f"Audit {id}: {action}"
